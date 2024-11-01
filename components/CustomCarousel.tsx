0fff95d57c4046f321281deedbd064c07404823f
Kamil Biczyk <Kamil.Biczyk@wilink.be>
2024-11-01 12:43:52 +0100

    reviews corrections

--- components/CustomCarousel.tsx
+++ components/CustomCarousel.tsx
@@ -23,7 +23,6 @@ interface CustomCarouselProps {
 }
 
 export default function CustomCarousel({ reviews }: CustomCarouselProps) {
-  console.log(reviews);
   const settings = {
     dots: true,
     infinite: true,
@@ -52,33 +51,49 @@ export default function CustomCarousel({ reviews }: CustomCarouselProps) {
     ],
   };
 
+  const renderStars = (rating: number) => {
+    return [...Array(5)].map((_, index) => (
+      <span key={index} className="text-yellow-400">
+        {index < rating ? "★" : "☆"}
+      </span>
+    ));
+  };
+
   if (!reviews || reviews.length === 0) {
     return null;
   }
 
   return (
-    <Slider {...settings}>
+    <Slider {...settings} className="cursor-grab">
       {reviews.map((review, index) => (
         // <a href={review.author_url} target="_blank" rel="noreferrer">
         <div key={index} className="carousel-elem px-5">
-          <p className="p-s text-bold text-white-b300 text-left">
-            {review.author_name}
-          </p>
-          <p className="p-xs text-left text-italic text-white-b300">
-            {review.relative_time_description}
-          </p>
-          <p className="mt-4 p-xs text-left text-white-b300">
+          <div className="flex items-center gap-4">
+            <div className="w-10 h-10 relative">
+              <Image
+                src={review.profile_photo_url}
+                alt={review.author_name}
+                layout="fill"
+                objectFit="contain"
+              />
+            </div>
+
+            <p className="p-s text-bold text-white-b300 text-left text-lg">
+              {review.author_name}
+            </p>
+          </div>
+
+          <div className="flex mt-1 gap-4">
+            <div>{renderStars(review.rating)}</div>{" "}
+            <p className="p-xs text-left text-italic text-white-b300">
+              {review.relative_time_description}
+            </p>
+          </div>
+
+          <p className="mt-1 p-xs text-left text-white-b300 pb-8">
             &quot;{review.text.slice(0, 200)}
             {review.text.length > 200 ? "..." : ""}&quot;
           </p>
-          <div className="w-10 h-10 relative mt-4">
-            <Image
-              src={review.profile_photo_url}
-              alt={review.author_name}
-              layout="fill"
-              objectFit="contain"
-            />
-          </div>
         </div>
         // </a>
       ))}
