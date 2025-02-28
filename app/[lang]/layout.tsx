c59589d4d6215b0c6262b1f83c15d9680ae3e666
kamilwilink <kamil.biczyk@wilink.be>
2025-02-28 15:38:53 +0100

    update

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Manrope } from "next/font/google";
+import { headers } from 'next/headers';
 
 /* COMPONENTS */
 import Header from "@/components/navigation/Header";
@@ -33,34 +34,39 @@ interface LayoutProps {
 
 export default async function RootLayout({ children, params }: LayoutProps) {
   const { lang } = await params;
+
+  const headersList = await headers();
+  const header_url = headersList.get('x-url') || "";
+  const navItems = [
+    {
+      href: `/${lang}`,
+      label: translate("1635f5c3-7f8a-42cd-a3b2-444c279f0b7c", lang),
+      active: header_url.endsWith(`/${lang}`),
+    },
+    {
+      href: `/${lang}/general-accounting`,
+      label: translate("9e52e1ec-ec33-4e9b-a6f8-6b9165411c1f", lang),
+      active: header_url.endsWith(`/${lang}/general-accounting`),
+    },
+    {
+      href: `/${lang}/about`,
+      label: translate("2ce9c735-c131-40bb-833d-5f1d16447c1c", lang),
+      active: header_url.endsWith(`/${lang}/about`),
+    },
+    {
+      href: `/${lang}/contact`,
+      label: translate("34db7e00-fd9d-44bd-9a7c-a16c9341c8f1", lang),
+      active: header_url.endsWith(`/${lang}/contact`),
+    },
+  ];
+
   return (
     <html lang={lang} className="scroll-smooth">
       <body className={`${manrope.variable} antialiased`}>
         <Header
           lang={lang}
           logo={logo}
-          elements={[
-            {
-              href: `/${lang}`,
-              label: translate("1635f5c3-7f8a-42cd-a3b2-444c279f0b7c", lang),
-              active: true,
-            },
-            {
-              href: `/${lang}/general-accounting`,
-              label: translate("9e52e1ec-ec33-4e9b-a6f8-6b9165411c1f", lang),
-              active: false,
-            },
-            {
-              href: `/${lang}/about`,
-              label: translate("2ce9c735-c131-40bb-833d-5f1d16447c1c", lang),
-              active: false,
-            },
-            {
-              href: `/${lang}/contact`,
-              label: translate("34db7e00-fd9d-44bd-9a7c-a16c9341c8f1", lang),
-              active: false,
-            },
-          ]}
+          elements={navItems}
         />
         {children}
         <Footer lang={lang} />
